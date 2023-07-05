2a7cd54e36501de736341fd889ae0e2ecc9af2d9
Sonja Sahebzad <123073089+SonjaJanssen@users.noreply.github.com>
2023-07-06 01:05:52 +0200

    Update plot 1.R

--- plot 1.R
+++ plot 1.R
@@ -2,7 +2,7 @@
 ##  plot1.R and plot1.png
 
 #   Lectures: By Professors PhD Jeffrey Leek, PhD Roger D. Peng, PhD Brian Caffo
-#   University: John Hopkins University Bloomberg School of Public Health _ at coursera
+#   University: John Hopkins University Bloomberg School of Public Health 
 #   Codes:  @Sonja Janssen-Sahebzad (@SonjaJS)
 #   Date:  16 April 2023
 
